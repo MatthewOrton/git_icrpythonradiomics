8c8e0a837ff15324263d82b1cef7e64c2ea2b4d6
Matthew Orton <matthew.orton@icr.ac.uk>
2021-03-31 15:17:07 +0100

    Added extra diagnostic fit
    Added GridSearchCV fit to all data to get resubstitution perfromance estimate, and best tuning parameters.
    
    Using best tuning parameters, so a cross-validated fit to see how much impact the nested CV has in terms of the variability of the tuning parameters between each outer CV fold.

--- machineLearning/nestedCVclassification.py
+++ machineLearning/nestedCVclassification.py
@@ -18,6 +18,8 @@ from sklearn.linear_model import LogisticRegression, PassiveAggressiveClassifier
 from sklearn.utils import resample
 import sys
 from plot_roc_cv import plot_roc_cv
+import copy
+from sklearn.pipeline import Pipeline
 
 def nestedCVclassification(X, y, estimators, *scoring, n_splits_inner=5, n_splits_outer=5, n_repeats=2, verbose=0, staircase=True, linewidth=2, color=None, plot_individuals=False):
 
@@ -49,9 +51,32 @@ def nestedCVclassification(X, y, estimators, *scoring, n_splits_inner=5, n_split
         n_jobs = 1
 
     for n, estimator in enumerate(estimators):
+
+        # get best tuning parameters from the whole data set
+        clf_best_params = GridSearchCV(estimator=estimator["model"], param_grid=estimator["p_grid"], cv=inner_cv, refit=True, verbose=0, scoring=estimator["scoring"])
+        clf_best_params.fit(X, y)
+        # make a model that uses the best tuning parameters  ...
+        model_best_params = copy.deepcopy(estimator["model"])
+        if isinstance(model_best_params, Pipeline):
+            for key in estimator["p_grid"].keys():
+                pp = key.split('__')
+                setattr(model_best_params.named_steps[pp[0]], pp[1], clf_best_params.best_params_[key])
+        else:
+            for key in estimator["p_grid"].keys():
+                setattr(model_best_params, key, clf_best_params.best_params_[key])
+        # ... and cross validate this model
+        # The purpose of this cross-validated fit is to see how much impact the parameter tuning is having on the performance, compared with the (correct) use
+        # of cross-validation for performance evaluation.  This fit does not use the nested cv loop for parameter tuning, so the variability caused by the different
+        # tuned parameters on the full nested cross-validation will not be included in this fit.
+        cv_result_best_params = cross_validate(model_best_params, X=X, y=y, cv=outer_cv, scoring=scoring, return_estimator=True, verbose=verbose, n_jobs=n_jobs)
+
+        # nested cross validation - the correct way to do things
         clf = GridSearchCV(estimator=estimator["model"], param_grid=estimator["p_grid"], cv=inner_cv, refit=True, verbose=0, scoring=estimator["scoring"])
         cv_result = cross_validate(clf, X=X, y=y, cv=outer_cv, scoring=scoring, return_estimator=True, verbose=verbose, n_jobs=n_jobs)
         estimators[n]["result"] = cv_result
+
+        for res in cv_result["estimator"]:
+            print(res.best_params_)
 
         # fig0, ax0 = plt.subplots(3, 1)
         # for ax, res in zip(ax0.reshape(-1), cv_result["estimator"]):
@@ -63,11 +88,23 @@ def nestedCVclassification(X, y, estimators, *scoring, n_splits_inner=5, n_split
         #     #ax.set_xscale("log")
         #     #ax.set_title(str(res.best_params_["C"]))
         # plt.show()
+        # fig0, ax0 = plt.subplots(3, 2)
+        # for ax, res in zip(ax0.reshape(-1), cv_result["estimator"]):
+        #     testScore = np.ma.getdata(res.cv_results_['mean_test_score']).astype('float')
+        #     # ax.plot(np.linspace(-4,4,20), testScore, color='blue')
+        #     # ax.plot(res.cv_results_['param_C'][res.cv_results_['param_penalty']=='l1'], testScore[res.cv_results_['param_penalty']=='l1'], color='blue')
+        #     # ax.plot(res.cv_results_['param_C'][res.cv_results_['param_penalty']=='l2'], testScore[res.cv_results_['param_penalty']=='l2'], color='red')
+        #     ax.plot(np.array(res.cv_results_['param_s__n_features_to_select']), testScore)
+        #     # ax.set_xscale("log")
+        #     # ax.set_title(str(res.best_params_["C"]))
+        # plt.show()
 
         # draw roc from outer cv
-        fig, ax = plt.subplots()
-        plot_roc_cv(X, y, outer_cv, cv_result, ax, plot_individuals=plot_individuals, smoothing=500, titleStr=estimator["name"], staircase=staircase, linewidth=linewidth, color=color)
-        ax.text(0, 1, 'AUC = ' + str(np.mean(cv_result['test_roc_auc']).round(3)) + ' \u00B1 ' + str(np.std(cv_result['test_roc_auc']).round(3)))
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+        plot_roc_cv(X, y, outer_cv, cv_result, ax1, plot_individuals=plot_individuals, smoothing=500, titleStr=estimator["name"]+' nested CV', staircase=staircase, linewidth=linewidth, color=color)
+        ax1.text(0, 1, 'AUC = ' + str(np.mean(cv_result['test_roc_auc']).round(3)) + ' \u00B1 ' + str(np.std(cv_result['test_roc_auc']).round(3)))
+        plot_roc_cv(X, y, outer_cv, cv_result_best_params, ax2, plot_individuals=plot_individuals, smoothing=500, titleStr=estimator["name"] + ' CV with best tuning param', staircase=staircase, linewidth=linewidth, color=color)
+        ax2.text(0, 1, 'AUC = ' + str(np.mean(cv_result_best_params['test_roc_auc']).round(3)) + ' \u00B1 ' + str(np.std(cv_result['test_roc_auc']).round(3)))
         plt.show()
 
         # tidy up nested_scores and key names
@@ -79,10 +116,29 @@ def nestedCVclassification(X, y, estimators, *scoring, n_splits_inner=5, n_split
         # output main result
         nested_scores_mean = {key: np.mean(value) for key, value in cv_result.items()}
         nested_scores_std = {key: np.std(value) for key, value in cv_result.items()}
+        print(' ')
         print(estimator["name"])
         print(nested_scores_mean)
         print(nested_scores_std)
         print(' ')
+        print('Resubstitution performance using parameters tuned on whole data set:')
+        print(clf_best_params.scoring + ' = ' + str(clf_best_params.score(X, y)))
+        print(' ')
+        print ('Best parameters tuned using whole data set:')
+        print(clf_best_params.best_params_)
+
+        # tidy up nested_scores and key names
+        cv_result_best_params.pop('fit_time', None)
+        cv_result_best_params.pop('score_time', None)
+        cv_result_best_params.pop('estimator', None)
+        cv_result_best_params = {key.replace('test_',''): value for key, value in cv_result_best_params.items()}
+
+        print(' ')
+        print('Cross-validated performance using the best tuning parameters based on GridSearchCV of whole data set:')
+        best_params_scores_mean = {key: np.mean(value) for key, value in cv_result_best_params.items()}
+        best_params_scores_std = {key: np.std(value) for key, value in cv_result_best_params.items()}
+        print(best_params_scores_mean)
+        print(best_params_scores_std)
 
     return estimators
 
