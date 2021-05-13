3130b06e75841b047e27e8ffde4069e122a1f454
Matthew Orton <matthew.orton@icr.ac.uk>
2021-05-13 12:29:01 +0100

    Create loadAndPreProcessRadiomicsFile.py
    Moved from NLSC_Immuno project

---
+++ machineLearning/loadAndPreProcessRadiomicsFile.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+from pyirr import intraclass_correlation
+from featureSelect_correlation import featureSelect_correlation
+from scipy.stats import spearmanr, skew
+
+def loadAndPreProcessRadiomicsFile(fileName, index_col=None, featureSelectStr='original', correlation_threshold=0.9, iccThreshold = None, followupStr='_followup', reproducibilityStr='_repro', logTransform=False):
+
+    # read data from file
+    df = pd.read_csv(fileName, index_col=index_col)
+
+    # remove non-data columns
+    df = df.loc[:, ~df.columns.str.contains('source|diagnostics')]
+
+    # Select chosen features (can be a regex, e.g. 'original|wavelet'
+    include = featureSelectStr
+    df = df.loc[:, df.columns.str.contains(include)]
+
+    # Most of the shape features are (non-linear) functions of MeshVolume and SurfaceArea.
+    # These extra features are useful for univariate feature discovery, but not for multivariate modelling as they are degenerate, and in a known way.
+    # MeshVolume and SurfaceArea are typically highly correlated, and so later correlation-based feature reductions would remove one of them, which is undesirable.
+    # Sphericity is derived from MeshVolume and SurfaceArea, is dimensionless, is interpretable, and tends to be uncorrelated with MeshVolume.
+    # This suggests keeping MeshVolume and Sphericity.
+    exclude = 'SurfaceArea|VoxelVolume|SurfaceVolumeRatio|Compactness1|Compactness2|SphericalDisproportion'
+    df = df.loc[:, ~df.columns.str.contains(exclude)]
+
+    # A similar argument applies to Major/Least/MinorAxisLength, Elongation and Flatness.
+    # In this case Elongation and Flatness are dimensionless so we discard MinorAxisLength and LeastAxisLength to remove the redundancy.
+    # Elongation and Flatness are often correlated with Sphericity, and the correlation feature reduction below will always remove Elongation and Flatness is this is the case.
+    exclude = 'MinorAxisLength|LeastAxisLength'
+    df = df.loc[:, ~df.columns.str.contains(exclude)]
+
+    # Use log transform on any features that only contain positive (or all negative) values, and where the skewness is lower on taking logs.
+    # Add '_log' subscript to column names so we can see which are transformed this way
+    if logTransform:
+        columnNames = df.columns
+        for column in columnNames:
+            # if any(column == : # skew and kurtosis exclude
+            #     continue
+            # Some columns may have NaNs etc, so leave these as they are, and just look at the finite values since
+            # we don't know here if we are going to use row/column deletion or data imputation to handle NaNs.
+            thisColumnFinite = np.isfinite(df[column])
+            thisColumn = df[column][thisColumnFinite]
+            sign = np.sign(thisColumn[0])
+            if np.all(np.sign(thisColumn) == sign):
+                logThisColumn = sign * np.log(sign * thisColumn)
+                # include sign so that all negative data (should it exist) will have the same ordering sense after taking logs
+                if np.abs(skew(logThisColumn)) < np.abs(skew(thisColumn)):
+                    df[column][thisColumnFinite] = logThisColumn
+                    columnNames = [x + '_log' if x == column else x for x in columnNames]
+        df.columns = columnNames
+
+    # split off rows for any followup scans
+    ifu = df.index.str.endswith(followupStr)
+    if any(ifu):
+        dfu = df.loc[ifu, :]
+        dfu = dfu.rename(index=lambda x: x.replace(followupStr, ''))
+        dfu.sort_index(inplace=True)
+        # remove from main data frame
+        df = df.loc[~ifu, :]
+    else:
+        dfu = []
+
+    # split off rows for any reproducibility data
+    ir = df.index.str.endswith(reproducibilityStr)
+    if any(ir):
+        dfr = df.loc[ir, :]
+        dfr = dfr.rename(index=lambda x: x.replace(reproducibilityStr, ''))
+        dfr.sort_index(inplace=True)
+        # remove from main data frame
+        df = df.loc[~ir, :]
+        # get rows of df corresponding to dfr
+        dfrr = df.loc[df.index.isin(dfr.index),:]
+        dfrr.sort_index(inplace=True)
+
+    # MeshVolume and Sphericity are the two principle shape features that are not removed in a previous step.
+    # If any other features are correlated with these two features then remove the other features in order
+    # to ensure MeshVolume and Sphericity remain as these are the easiest features to interpret.
+    corrMat = np.abs(spearmanr(np.array(df)).correlation)
+    iMajorAxisLength = np.where(df.columns.str.contains('original_shape_MajorAxisLength'))[0]
+    iMaximum3DDiameter = np.where(df.columns.str.contains('original_shape_Maximum3DDiameter'))[0]
+    iMeshVolume = np.where(df.columns.str.contains('original_shape_MeshVolume'))[0]
+    iSphericity = np.where(df.columns.str.contains('original_shape_Sphericity'))[0]
+    ind = np.logical_and(corrMat[:, iMeshVolume] < correlation_threshold, corrMat[:, iSphericity] < correlation_threshold)
+    ind[iMeshVolume] = True
+    ind[iSphericity] = True
+    # Check if MajorAxisLength and Maximum3DDiameter are correlated, and force MajorAxisLength to be removed.
+    # Maximum3DDiameter may also be removed if it is correlated with MeshVolume, but this is not handled explicitly
+    if np.abs(corrMat[iMajorAxisLength, iMaximum3DDiameter])>correlation_threshold:
+        ind[iMajorAxisLength] = False
+    df = df.loc[:, ind[:, 0]]
+
+    # Apply generic pair-wise correlation feature reduction.
+    # Since we previously removed features correlated with MeshVolume and Sphericity, these two features
+    # are guaranteed to survive this step.
+    fsc = featureSelect_correlation(threshold=correlation_threshold, exact=True)
+    fsc.fit(np.array(df))
+    df = df.loc[:, fsc._get_support_mask()]
+
+    # compute reproducibility statistics and remove non-reproducible features
+    icc = {}
+    if 'dfr' in  locals():
+        Iuse = np.ones(len(df.columns)).astype(bool)
+        for n, col in enumerate(df.columns):
+            data = np.stack((dfr[col], dfrr[col]), axis=1)
+            icc[col] = intraclass_correlation(data, "twoway", "agreement").value
+            if iccThreshold is not None:
+                Iuse[n] = icc[col] > iccThreshold
+        df = df.loc[:, Iuse]
+
+    # make columns of dfu match df
+    if len(dfu)>0:
+        dfu = dfu[df.columns]
+
+    return df, dfu, icc
