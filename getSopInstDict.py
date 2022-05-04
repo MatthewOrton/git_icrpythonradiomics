577cf98715d4c43c34c3bbd904334dd590a097ce
Matthew Orton <matthew.orton@icr.ac.uk>
2022-05-04 20:19:12 +0100

    Check if more than one acquisition number in series

--- getSopInstDict.py
+++ getSopInstDict.py
@@ -22,9 +22,13 @@ def getSopInstDict(path):
             sopInstToSopClassUidDict = unpickle['sopInstToSopClassUidDict']
             instanceNumberDict = unpickle['instanceNumberDict']
             sopInst2instanceNumberDict = unpickle['sopInst2instanceNumberDict']
+            AcquisitionNumberForChecking = unpickle['AcquisitionNumberForChecking']
 
     # make list of files that aren't in the pre-computed dictionary
     files = list(set(glob.glob(os.path.join(path, '**'), recursive=True)) - set(list(sopInstDict.values())))
+
+    if 'AcquisitionNumberForChecking' not in locals():
+        AcquisitionNumberForChecking = {}
 
     if len(files)>0:
         print('Updating SOPInstanceUID dictionary:')
@@ -32,6 +36,17 @@ def getSopInstDict(path):
             for n, imageFile in enumerate(files):
                 if not os.path.isdir(imageFile) and pydicom.misc.is_dicom(imageFile):
                     dcm = pydicom.dcmread(imageFile)
+
+                    # Currently the AcquisitionNumber is not used to organise the output data structures, it's just recorded
+                    # so this code assumes the InstanceNumber is all that is needed to organise the hierarchy, but this is not correct!!
+                    # For now just record all the AcquistionNumbers and flag up if there is >1 AcquisitionNumber in any series
+                    if hasattr(dcm, 'AcquisitionNumber') and dcm.AcquisitionNumber:
+                        acq = int(dcm.AcquisitionNumber)
+                    else:
+                        acq = int(-1)
+                    if dcm.SeriesInstanceUID not in AcquisitionNumberForChecking:
+                        AcquisitionNumberForChecking[dcm.SeriesInstanceUID] = []
+                    AcquisitionNumberForChecking[dcm.SeriesInstanceUID].append(acq)
 
                     sopInstDict[str(dcm.SOPInstanceUID)] = imageFile
                     sopInstToSopClassUidDict[str(dcm.SOPInstanceUID)] = str(dcm.SOPClassUID)
@@ -57,12 +72,16 @@ def getSopInstDict(path):
         print('Complete')
         print(' ')
 
+    for seriesUID, acqList in AcquisitionNumberForChecking.items():
+        if len(set(acqList))>1:
+            print('\033[1m\033[94mWARNING getSopInstDict(): Series has AcquisitionNumbers ' + str(list(set(acqList))) + ' SeriesInstanceUID = ' + str(seriesUID) + '\033[0m\033[0m')
+
     # remove from dictionary any files that aren't found on file system
     filesNotPresent = list(set(list(sopInstDict.values())) - set(glob.glob(os.path.join(path, '**'), recursive=True)))
     sopInstDict = {key: value for key, value in sopInstDict.items() if value not in filesNotPresent}
 
     if len(sopInstDict)>0:
         with open(quickLoad, 'wb') as f:
-            pickle.dump({'sopInstDict':sopInstDict, 'sopInstToSopClassUidDict':sopInstToSopClassUidDict, 'instanceNumberDict':instanceNumberDict, 'sopInst2instanceNumberDict':sopInst2instanceNumberDict}, f)
+            pickle.dump({'sopInstDict':sopInstDict, 'sopInstToSopClassUidDict':sopInstToSopClassUidDict, 'instanceNumberDict':instanceNumberDict, 'sopInst2instanceNumberDict':sopInst2instanceNumberDict, 'AcquisitionNumberForChecking':AcquisitionNumberForChecking}, f)
 
     return sopInstDict, sopInstToSopClassUidDict, instanceNumberDict, sopInst2instanceNumberDict
