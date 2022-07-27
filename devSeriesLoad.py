3d9679e469509eac809b2b98893b2de0d59360aa
Matthew Orton <matthew.orton@icr.ac.uk>
2022-07-27 11:32:45 +0100

    Update devSeriesLoad.py

--- devSeriesLoad.py
+++ devSeriesLoad.py
@@ -5,25 +5,19 @@ import pydicom
 import numpy as np
 from skimage import draw
 import matplotlib.pyplot as plt
+from dataLoader import dataLoader
 
 
 
+rtsFile = '/Users/morton/Dicom Files/BRC_GCT/XNAT/assessors/BRC_GCT_RMH_002__II__20110608_132500_BrightSpeed__II__2__II__AIM_20220307_085805.dcm'
 
+seriesFolderDict = {'1.2.752.24.3.327953459.6497310.59202072':'/Users/morton/Dicom Files/BRC_GCT/XNAT/referencedScans/BRC_GCT_RMH_002__II__20110608_132500_BrightSpeed'}
 
-rts = pydicom.dcmread('/Users/morton/Dicom Files/BRC_GCT/XNAT/assessors/BRC_GCT_RMH_002__II__20110608_132500_BrightSpeed__II__2__II__AIM_20220307_085805.dcm')
+data = dataLoader(rtsFile, seriesFolderDict)
 
-rfors = rts.ReferencedFrameOfReferenceSequence[0]
-rtrss = rfors.RTReferencedStudySequence[0]
-ReferencedSeriesUID = rtrss.RTReferencedSeriesSequence[0].SeriesInstanceUID
-
-folder = '/Users/morton/Dicom Files/BRC_GCT/XNAT/referencedScans/BRC_GCT_RMH_002__II__20110608_132500_BrightSpeed'
-
-series = loadDicomSeries(ReferencedSeriesUID, folder, maxSpatiallyNonCompatibleInstances=1)
-
-series = readRTSdata(rts, series)
-
-image, mask_Lesion1 = getNamedROI(series, 'Lesion1')
-_, mask_Lesion1_hole = getNamedROI(series, 'Lesion1_hole')
+image, mask_Lesion1 = data.getNamedROI('Lesion1')
+_, mask_Lesion1_hole = data.getNamedROI('Lesion1_hole')
+_, mask_Lesion2 = data.getNamedROI('Lesion2')
 
 mask_Lesion1 = np.logical_and(mask_Lesion1, np.logical_not(mask_Lesion1_hole))
 
